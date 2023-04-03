deea520702bf2eae4a07881edd6b2e936ad8391e
yeeun <22000546@handong.ac.kr>
2023-04-03 23:18:01 +0900

    Add 65_2133.cpp

--- problems/part1/65_2133.cpp
+++ problems/part1/65_2133.cpp
@@ -6,16 +6,19 @@ using namespace std;
 int main() {
     int n;
     cin >> n;
-    vector<int> arr(n+1);
-    for(int i = 1; i <= n; i += 2) {
-        arr[i] = 0;
-    }
+
+    vector<int> arr(n+1, 0);
+
+    arr[0] = 1;
     arr[2] = 3;
-
+    for(int i = 4; i <= n; i += 2) {
+        arr[i] = arr[i-2] * 3;
+        for(int j = 4; j <= i; j ++) {
+            arr[i] += 2*arr[i-j];
+        }
+    }
     
-
-
-
+    cout << arr[n] << endl;
 
     return 0;
 }
